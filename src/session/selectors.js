1b86c3fab6df0e2f132d367d46baa2041a0e8f9e
Seth Lowie <seth.lowie@gmail.com>
2020-06-24 19:05:36 -0400

    remove generated ts files

--- src/session/selectors.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.sessionSelector = void 0;
-exports.sessionSelector = function (state) {
-    return state.session;
-};
--- src/session/types.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.SET_SESSION = exports.LOGIN = void 0;
-exports.LOGIN = "LOGIN";
-exports.SET_SESSION = "SET_SESSION";
